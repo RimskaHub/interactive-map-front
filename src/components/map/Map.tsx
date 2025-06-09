5d9b976feaabb2180a005ce6fdbebaf260690511
neven <nevenmitrovic4@gmail.com>
2025-06-09 12:55:58 +0200

    implementation of react icons in leaflet map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,5 +1,31 @@
+import { renderToString } from 'react-dom/server';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { divIcon } from 'leaflet';
+import { IoLocation } from 'react-icons/io5';
+
 import 'leaflet/dist/leaflet.css';
+
+// mock api response for locations
+const mockMarkers: { geocode: [number, number]; popupText: string }[] = [
+  {
+    geocode: [51.505, -0.09],
+    popupText: 'London Eye - Famous observation wheel offering panoramic views of London',
+  },
+  {
+    geocode: [51.509, -0.118],
+    popupText: 'Big Ben - Iconic clock tower and symbol of London',
+  },
+  {
+    geocode: [51.501, -0.142],
+    popupText: 'Buckingham Palace - Official residence of the British Royal Family',
+  },
+];
+const customIcon = divIcon({
+  html: renderToString(<IoLocation size={30} color="var(--clr-slate-600)" />),
+  className: 'custom-div-icon',
+  iconSize: [30, 30],
+  iconAnchor: [15, 30],
+});
 
 const Map = () => {
   return (
@@ -8,11 +34,14 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+      {mockMarkers.map((marker) => {
+        return (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popupText}</Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
   );
 };
